5e9db4c6008958830c9a28537e8582c205c8d414
nickcastro <castro.a.nicholas@gmail.com>
2016-03-04 23:13:27 -0500

    Added files via upload

--- Counter.cpp
+++ Counter.cpp
@@ -8,79 +8,82 @@ using namespace std;
 
 int main() {
 
-char get;
+int numLetters = 0; // length of word to be guessed
+int run = 1; // flag variable to determine if the program should continue
 char alpha[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-int alphaCount[26] = {0};
-int alphaWordCount[26] = {0};
-int uniqueLetterAppearance[26] = {0};
-int excludeLetters[26] = {0};
-int ndx, ndx2;
-int numLetters = 0, max_letter_count = 0;
-
-vector<string> initial_wordList; 
-vector<string> refined_wordList;
-vector<string> tmp_wordList;
-
-int numLetters_words;
+int alphaCount[26] = {0}; // array of letter frequency count (non unique letters per word)
+int alphaWordCount[26] = {0}; // array of unique letters per word
+int uniqueLetterAppearance[26] = {0}; // array to flag if there is a duplicate letter
+int excludeLetters[26] = {0}; // array to flag which letters should not be excluded from consideration
+int i, j, k; // index variables
+int max_letter_count = 0; // count of max letter frequency
+int positions_known = 0; // count of known positions within word
+int word_count; // number of possible words in main list 
 
 string word;
+vector<string> wordList; // main list of possible words
+vector<string> tmp_wordList; // tmp list used to clear wordList and repopulate with refined words
+
 long int Counter = 0, Counter1= 0, count = 0, count1 = 0;
 
 char tried[26]; // array of guesses (max 26)
 int guess_index = 0; // running number of guesses
+int miss = 0; // number of missed guesses
 
 string in_word; // decision variable for whether or not (y/n) the letter is in the word
 int correct_letter_count; // position(s) of correctly guessed letters in word
-vector<int> position;
+vector<int> position; // list of positions of correctly guessed words
 
-int tmp_position;
+int tmp_position; // used to populate position vector
 
-int add_word_flag = 0;
-int test=0, test2=0;
+int add_word_flag = 0; // flag to indicate if a word should be added to a newly populated wordList
+int test=0; // will deleted later
+
 
 cout << "How many letters? ";
 cin >> numLetters;
-
 ifstream in_s;
-
 in_s.open("dictionary.txt");
 
 
-// create initial list of 'numLetters' length words 
-while(in_s >> word) {
+// create vector of words of length numLetters
+while(in_s >> word){
 	if(word.length() == numLetters) {
 		word[0] = tolower(word[0]);
-		
-		for(count=0;count<numLetters;count++) {
-			get = word[count];
-
-			// this loop works the same way as the original switch statement
-			// count letter occurance frequency of each word
-			for(ndx=0;ndx<26;ndx++){
-				if(get == alpha[ndx]){
-					alphaCount[ndx]++;
-					uniqueLetterAppearance[ndx] = 1; // count non-duplicate letters of each word
-				} 
-			}
-
-		}
-
-		// count number of words each letter appears in
-		for(ndx=0;ndx<26;ndx++){
-			if(uniqueLetterAppearance[ndx] == 1){
-				alphaWordCount[ndx]++;
-			}
-			uniqueLetterAppearance[ndx] = 0; // reset flag variable
-		}
-
-		// if word is of correct length, add to wordList
-		initial_wordList.push_back(word);
+		wordList.push_back(word); // if correct length, add to wordList
 		Counter++;
-	
 	}
 }
 
 
+while(run){
+
+cout << Counter << endl;
+word_count = wordList.size();
+
+// count overall frequency of letters and frequency of unique letters per word
+for(i=0;i<word_count;i++) {		
+	for(j=0;j<numLetters;j++) {
+
+		// this loop works the same way as the original switch statement
+		// count letter occurance frequency of each word
+		for(k=0;k<26;k++){
+			if(wordList[i][j] == alpha[k]){
+				if(excludeLetters[k] != 1){
+					alphaCount[k]++;
+					uniqueLetterAppearance[k] = 1; // count non-duplicate letters of each word
+				}
+			} 
+		}
+	}
+	// count number of words each letter appears in
+	for(k=0;k<26;k++){
+		if(uniqueLetterAppearance[k] == 1){
+			alphaWordCount[k]++;
+		}
+		uniqueLetterAppearance[k] = 0; // reset flag variable
+	}
+}
 
 for(count1=0;count1<26;count1++) {
 	if(alphaCount[count1] > max_letter_count) {
@@ -88,18 +91,15 @@ for(count1=0;count1<26;count1++) {
 	}
 }
 
-cout << Counter << " " << numLetters << " letter words." << endl;
-numLetters_words = initial_wordList.size();
-cout << "numLetters words = " << numLetters_words << endl;
-
-cout << "first word: " << initial_wordList[0] << endl;
-cout << "last word: " << initial_wordList[Counter - 1] << endl;
+cout << "word count: " << word_count << " " << numLetters << " letter words." << endl;
+cout << "first word: " << wordList[0] << endl;
+cout << "last word: " << wordList[word_count - 1] << endl;
 
 
 // print all occurances of letters
-for(ndx=0;ndx<26;ndx++){
-	cout << alpha[ndx] << " found " << alphaCount[ndx] << " times in " << alphaWordCount[ndx] << " words " << endl;
-	cout << "Relative probability of duplicate letters: " << (static_cast<float>(alphaCount[ndx])/static_cast<float>(alphaWordCount[ndx])) - 1 << endl;
+for(i=0;i<26;i++){
+	cout << alpha[i] << " found " << alphaCount[i] << " times in " << alphaWordCount[i] << " words " << endl;
+	cout << "Relative probability of duplicate letters: " << (static_cast<float>(alphaCount[i])/static_cast<float>(alphaWordCount[i])) - 1 << endl;
 	cout << endl;
 }
 
@@ -109,8 +109,10 @@ for(count1=0;count1<26;count1++) {
 	if(alphaCount[count1] == max_letter_count) {
 		cout << "Guess X should be the letter " << "'" << alpha[count1] << "'" << endl;
 		tried[guess_index] = alpha[count1];
+		excludeLetters[count1] = 1; // this letter should not be considered in future guesses
 	}
 }
+
 
 
 cout << "is the letter " << "'" << tried[guess_index] << "'" << " in the word? (y/n)" << endl;
@@ -119,31 +121,28 @@ cin >> in_word;
 if(in_word == "y"){
 	cout << "How many times does the letter " << "'" << tried[guess_index] << "'" << " occur?" << endl;
 	cin >> correct_letter_count;
-	for(ndx=0; ndx < correct_letter_count; ndx++){
-		cout << "occurance " << ndx+1 << " is at position: " << endl;
+	for(i=0; i < correct_letter_count; i++){
+		cout << "occurance " << i+1 << " is at position: " << endl;
 		cin >> tmp_position;
 		position.push_back(tmp_position);
 
 	}
-
 	cout << "The letter " << "'" << tried[guess_index] << "'" << " appears at positions: " << endl;
-	for(ndx=0;ndx<correct_letter_count;ndx++){
-		cout << position[ndx] << endl;
+	for(i=0;i<correct_letter_count;i++){
+		cout << position[i+positions_known] << endl;
 	}
 
 } else{
-	excludeLetters[count1] = 1; // this letter is a miss
+	miss++;
 }
 
-
-
-for(ndx=0;ndx<numLetters_words;ndx++) {
+for(i=0;i<word_count;i++) {
 	test++;
-		for(ndx2=0; ndx2 < correct_letter_count; ndx2++){
-			if(initial_wordList[ndx][position[ndx2]-1] == tried[guess_index]){
+		for(j=0;j<correct_letter_count;j++){
+			if(wordList[i][position[j+positions_known]-1] == tried[guess_index]){
 				add_word_flag++;
 				if(add_word_flag == correct_letter_count){
-        			refined_wordList.push_back(initial_wordList[ndx]);
+        			tmp_wordList.push_back(wordList[i]);
         			Counter1++;
         		}
 			}
@@ -153,15 +152,38 @@ for(ndx=0;ndx<numLetters_words;ndx++) {
 
 cout << endl;
 cout << test << " " << tried[guess_index] << endl;
-cout << test2 << " words of length " << numLetters << endl;
-cout << Counter1 << " words after filter "<< guess_index+1 << endl;
-for(ndx=0;ndx<Counter1;ndx++){
-	cout << refined_wordList[ndx] << endl;
+cout << Counter1 << " words after filter "<< guess_index+2 << endl;
+
+
+// set wordlist = to list of refined words
+// reset letter frequency counters and word counter
+// increment count of letters positions known and guess index
+
+wordList.clear();
+for(i=0;i<Counter1;i++){
+	wordList.push_back(tmp_wordList[i]);
+}
+tmp_wordList.clear();
+for(i=0;i<Counter1;i++){
+	cout << wordList[i] << endl;
 }
 
+Counter1 = 0;
+max_letter_count = 0;
+for(i=0;i<26;i++){
+	alphaCount[i] = 0;
+	alphaWordCount[i] = 0;
+	uniqueLetterAppearance[i] = 0;
+}
+guess_index++;
+positions_known += correct_letter_count;
 
 
+if(miss == 6 || word_count == 1){
+	run = 0;
+}
 
+}
 
 in_s.close();
 
